873fef970cefa001215fc51e7793e8c728badec4
sandrewtx08 <sandrewtx08@protonmail.com>
2022-03-29 17:50:19 -0300

    token middleware

--- src/token.ts
+++ src/token.ts
@@ -7,29 +7,28 @@ import {
   JsonWebTokenError,
 } from "jsonwebtoken";
 
+export let ACCESS_EXP = config.JWT_ACCESS_TOKEN_EXP;
+export let ACCESS = config.JWT_ACCESS_TOKEN;
+export let REFRESH_EXP = config.JWT_REFRESH_TOKEN_EXP;
+export let REFRESH = config.JWT_REFRESH_TOKEN;
+
 export function signAccessToken(refresh_id: string, user: User) {
-  return sign({ refresh_id, user_id: user.id }, config.JWT_ACCESS_TOKEN, {
-    expiresIn: String(config.JWT_ACCESS_TOKEN_EXP),
+  return sign({ refresh_id, user_id: user.id }, ACCESS, {
+    expiresIn: String(ACCESS_EXP),
   });
 }
 
 export function signRefreshToken(uuid: string, user: User) {
-  return sign(
-    { refresh_id: uuid, user_id: user.id },
-    config.JWT_REFRESH_TOKEN,
-    {
-      expiresIn: String(config.JWT_REFRESH_TOKEN_EXP),
-    }
-  );
+  return sign({ refresh_id: uuid, user_id: user.id }, REFRESH, {
+    expiresIn: String(REFRESH_EXP),
+  });
 }
 
 export function verifyAccessToken(token: string) {
   try {
-    let payload = Object(
-      verify(token, config.JWT_ACCESS_TOKEN, { ignoreExpiration: true })
-    );
+    let payload = Object(verify(token, ACCESS, { ignoreExpiration: true }));
     try {
-      payload = Object(verify(token, config.JWT_ACCESS_TOKEN));
+      payload = Object(verify(token, ACCESS));
       return { expired: false, ...payload };
     } catch (err) {
       if (err instanceof TokenExpiredError)
@@ -44,12 +43,12 @@ export function verifyAccessToken(token: string) {
 export function verifyRefreshToken(user: User) {
   try {
     let payload = Object(
-      verify(user.refresh_token, config.JWT_REFRESH_TOKEN, {
+      verify(user.refresh_token, REFRESH, {
         ignoreExpiration: true,
       })
     );
     try {
-      payload = Object(verify(user.refresh_token, config.JWT_REFRESH_TOKEN));
+      payload = Object(verify(user.refresh_token, REFRESH));
       return { expired: false, ...payload };
     } catch (err) {
       if (err instanceof TokenExpiredError)
