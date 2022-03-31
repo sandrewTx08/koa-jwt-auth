09598fc0fd898521c988f065d90a474be74ad2ed
sandrewtx08 <sandrewtx08@protonmail.com>
2022-03-31 12:32:18 -0300

    organizing test context

--- test/User.controller.test.js
+++ test/User.controller.test.js
@@ -1,7 +1,7 @@
 const { assert } = require("console");
 const { request } = require("./index");
 
-describe("user controller", () => {
+describe("User controller", () => {
   let createOneCredentials = {
     username: "testUsername",
     password: "testPassword",
@@ -16,8 +16,8 @@ describe("user controller", () => {
   };
   let updatedOne;
 
-  describe("User controller", () => {
-    it("createOne", () => {
+  describe("createOne", () => {
+    it("create user", () => {
       return request
         .post("/v1/user")
         .send({
@@ -33,7 +33,7 @@ describe("user controller", () => {
         });
     });
 
-    it("createOne duplicate", () => {
+    it("create user duplicate", () => {
       return request
         .post("/v1/user")
         .send({
@@ -50,53 +50,57 @@ describe("user controller", () => {
       return request.get(`/v1/user/${createOne.id}`).expect(200);
     });
 
-    it("authenticatePassword incorrect password after create", () => {
-      return request
-        .post(`/v1/login`)
-        .send({
-          username: createOneCredentials.username,
-          password: "incorrect password",
-        })
-        .set("Content-Type", "application/json")
-        .set("Accet", "application/json")
-        .expect(400);
-    });
+    describe("authenticatePassword", () => {
+      it("incorrect password", () => {
+        return request
+          .post(`/v1/login`)
+          .send({
+            username: createOneCredentials.username,
+            password: "incorrect password",
+          })
+          .set("Content-Type", "application/json")
+          .set("Accet", "application/json")
+          .expect(400);
+      });
 
-    it("authenticatePassword with email after create", () => {
-      return request
-        .post(`/v1/login`)
-        .send({
-          email: createOneCredentials.email,
-          password: createOneCredentials.password,
-        })
-        .set("Content-Type", "application/json")
-        .set("Accet", "application/json")
-        .expect(200)
-        .then((res) => {
-          assert(res.body.id === createOne.id);
-        });
-    });
+      it("login by email", () => {
+        return request
+          .post(`/v1/login`)
+          .send({
+            email: createOneCredentials.email,
+            password: createOneCredentials.password,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accet", "application/json")
+          .expect(200)
+          .then((res) => {
+            assert(res.body.id === createOne.id);
+          });
+      });
 
-    it("authenticatePassword with username after create", () => {
-      return request
-        .post(`/v1/login`)
-        .send({
-          username: createOneCredentials.username,
-          password: createOneCredentials.password,
-        })
-        .set("Content-Type", "application/json")
-        .set("Accet", "application/json")
-        .expect(200)
-        .then((res) => {
-          assert(res.body.id === createOne.id);
-        });
+      it("login by username", () => {
+        return request
+          .post(`/v1/login`)
+          .send({
+            username: createOneCredentials.username,
+            password: createOneCredentials.password,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accet", "application/json")
+          .expect(200)
+          .then((res) => {
+            assert(res.body.id === createOne.id);
+          });
+      });
     });
 
     it("findAll", () => {
       return request.get("/v1/user").expect(200);
     });
+  });
 
-    it("findOneAndUpdate", () => {
+  describe("findOneAndUpdate", () => {
+    it("update user credentials", () => {
       return request
         .patch(`/v1/user/${createOne.id}`)
         .send({
@@ -112,85 +116,95 @@ describe("user controller", () => {
         });
     });
 
-    it("authenticatePassword incorrect password after update", () => {
-      return request
-        .post(`/v1/login`)
-        .send({
-          email: updateOneCredentials.email,
-          password: "incorrect password",
-        })
-        .set("Content-Type", "application/json")
-        .set("Accet", "application/json")
-        .expect(400);
-    });
-
-    it("authenticatePassword with email after update", () => {
-      return request
-        .post(`/v1/login`)
-        .send({
-          email: updateOneCredentials.email,
-          password: updateOneCredentials.password,
-        })
-        .set("Content-Type", "application/json")
-        .set("Accet", "application/json")
-        .expect(200)
-        .then((res) => {
-          assert(res.body.id === createOne.id);
-        });
-    });
-
-    it("authenticatePassword with username after update", () => {
-      return request
-        .post(`/v1/login`)
-        .send({
-          username: updateOneCredentials.username,
-          password: updateOneCredentials.password,
-        })
-        .set("Content-Type", "application/json")
-        .set("Accet", "application/json")
-        .expect(200)
-        .then((res) => {
-          assert(res.body.id === createOne.id);
-        });
-    });
-
-    it("findOneAndUpdate equal username", () => {
-      return request
-        .patch(`/v1/user/${updatedOne.id}`)
-        .send({
-          username: updateOneCredentials.username,
-        })
-        .set("Content-Type", "application/json")
-        .set("Accept", "application/json")
-        .expect(400);
-    });
-
-    it("findOneAndUpdate equal email", () => {
-      return request
-        .patch(`/v1/user/${updatedOne.id}`)
-        .send({
-          email: updateOneCredentials.email,
-        })
-        .set("Content-Type", "application/json")
-        .set("Accept", "application/json")
-        .expect(400);
-    });
-
-    it("findOneAndUpdate equal password", () => {
-      return request
-        .patch(`/v1/user/${updatedOne.id}`)
-        .send({
-          password: updateOneCredentials.password,
-        })
-        .set("Content-Type", "application/json")
-        .set("Accept", "application/json")
-        .expect(400);
-    });
-
     it("findOne after update", () => {
       return request.get(`/v1/user/${updatedOne.id}`).expect(200);
     });
 
+    describe("authenticatePassword", () => {
+      it("incorrect password", () => {
+        return request
+          .post(`/v1/login`)
+          .send({
+            email: updateOneCredentials.email,
+            password: "incorrect password",
+          })
+          .set("Content-Type", "application/json")
+          .set("Accet", "application/json")
+          .expect(400);
+      });
+
+      it("login by email", () => {
+        return request
+          .post(`/v1/login`)
+          .send({
+            email: updateOneCredentials.email,
+            password: updateOneCredentials.password,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accet", "application/json")
+          .expect(200)
+          .then((res) => {
+            assert(res.body.id === createOne.id);
+          });
+      });
+
+      it("login by username", () => {
+        return request
+          .post(`/v1/login`)
+          .send({
+            username: updateOneCredentials.username,
+            password: updateOneCredentials.password,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accet", "application/json")
+          .expect(200)
+          .then((res) => {
+            assert(res.body.id === createOne.id);
+          });
+      });
+    });
+
+    describe("equal credentials", () => {
+      it("username", () => {
+        return request
+          .patch(`/v1/user/${updatedOne.id}`)
+          .send({
+            username: updateOneCredentials.username,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json")
+          .expect(400);
+      });
+
+      it("email", () => {
+        return request
+          .patch(`/v1/user/${updatedOne.id}`)
+          .send({
+            email: updateOneCredentials.email,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json")
+          .expect(400);
+      });
+
+      it("password", () => {
+        return request
+          .patch(`/v1/user/${updatedOne.id}`)
+          .send({
+            password: updateOneCredentials.password,
+          })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json")
+          .expect(400);
+      });
+    });
+
+    it("findAll", () => {
+      return request.get("/v1/user").expect(200);
+    });
+  });
+
+  describe("findOneAndDelete", () => {
     it("findOneAndDelete", () => {
       return request.delete(`/v1/user/${updatedOne.id}`).expect(200);
     });
